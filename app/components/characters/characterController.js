a4eee5df8d800241b10c25488a77bcba11f50e03
Tim Sarvey <timsarvey@gmail.com>
2016-01-18 15:33:58 -0500

    Add 'get more' pagination feature.

--- app/components/characters/characterController.js
+++ app/components/characters/characterController.js
@@ -1,12 +1,22 @@
 angular.module('StarWarsApp')
 	.controller('characterController', ['$scope', '$http', 'characterFactory', function($scope, $http, characterFactory){
 
-		characterFactory.getCharacters(function(err, people) {
+		characterFactory.getAllCharacters(function(err, people) {
             if(err) {
                 return console.log(err);
             }
             $scope.people = people;
             console.log(people);
         });
+
+        $scope.getMoreCharacters = function(){
+        	characterFactory.getAllCharacters(function(err, people) {
+        		if(err) {
+        			return console.log(err);
+        		}
+        		$scope.people = people;
+        		console.log(people);
+        	});
+        };
          
 	}]);
--- build/app.js
+++ build/app.js
@@ -2,59 +2,69 @@ angular.module('StarWarsApp', ['lumx']);
 angular.module('StarWarsApp')
 	.controller('characterController', ['$scope', '$http', 'characterFactory', function($scope, $http, characterFactory){
 
-		characterFactory.getCharacters(function(err, people) {
+		characterFactory.getAllCharacters(function(err, people) {
             if(err) {
                 return console.log(err);
             }
             $scope.people = people;
             console.log(people);
         });
+
+        $scope.getMoreCharacters = function(){
+        	characterFactory.getAllCharacters(function(err, people) {
+        		if(err) {
+        			return console.log(err);
+        		}
+        		$scope.people = people;
+        		console.log(people);
+        	});
+        };
          
 	}]);
 angular.module('StarWarsApp')
 	.factory('characterFactory', ['$http', 'titleCase', function($http, titleCase){
 
-		console.log(titleCase);
-
-		function getHomeworldName(url, index, people){
-			$http.get(url).then(function(response){
-				people[index].homeworld = response.data.name;
-			}, function(){
-				people[index].homeworld = "error";
-			});
-		}
-
-		function getFilmNames(url, index, people){
-			$http.get(url).then(function(response){
-				people[index].films = response.title;
-			}, function(){
-				people[index].films = "error";
-			});
-		}
-		
+		var pageNumber = 1;
+		var people = [];
 
 		return {
-			getCharacters: function(callback)	{
-				$http.get('http://swapi.co/api/people/')
+			getAllCharacters: function(callback)	{
+				console.log('function called');
+				$http.get('http://swapi.co/api/people/?page=' + pageNumber)
 					.then(function(response) {
-						console.log(response);
 						var peopleResponse = response.data.results;
-						var people = [];
+						var totalPeople = people.length;
 						for (x = 0; x < peopleResponse.length; x++){
-							people[x] = {
+							people[x + totalPeople] = {
 								name: peopleResponse[x].name,
 								birth_year: peopleResponse[x].birth_year,
 								hair_color: titleCase(peopleResponse[x].hair_color),
 								skin_color: titleCase(peopleResponse[x].skin_color),
 								gender: titleCase(peopleResponse[x].gender),
 								height: peopleResponse[x].height + "cm",
-								mass: peopleResponse[x].mass + "kg",
+								mass: peopleResponse[x].mass + "kg"
 							};
-							getHomeworldName(peopleResponse[x].homeworld, x, people);
 						} // end for loop
+						pageNumber++;
 						callback(null, people);
 					}, function(err) {
 						callback(err);
+				});
+			},
+
+			getCharacterByUrl: function(callback, url){
+				$http.get(url).then(function(response){
+					var character = {
+						name: response.name,
+						height: response.height + 'cm',
+						mass: response.mass + 'kg',
+						hair_color: titleCase(response.hair_color),
+						skin_color: titleCase(response.skin_color),
+						gender: titleCase(response.gender)
+					};
+					callback(null, character);
+				}, function(err){
+					callback(err);
 				});
 			}
 		};
