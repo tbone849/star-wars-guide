e59386de382122d9c34532e7ca353970f2abf422
Tim Sarvey <timsarvey@gmail.com>
2016-01-22 16:14:28 -0500

    Add pagination directive attributes.

--- app/directives/pagination.js
+++ app/directives/pagination.js
@@ -2,6 +2,14 @@ angular.module('StarWarsApp')
 	.directive('pagination', function(){
 		return {
 			restrict: 'E',
-			templateUrl: '/directives/pagination.html'
+			templateUrl: '/directives/pagination.html',
+			scope: {
+				currentPage: "=",
+				pages: "=",
+				action: "&clickAction"
+			},
+			link: function(scope, element, attr){
+				console.log(scope);
+			}
 		};
 	});
