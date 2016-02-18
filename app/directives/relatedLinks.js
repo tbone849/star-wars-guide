3dc18e5440e29c202987d71c6c032579f2fc296d
Tim Sarvey <timsarvey@gmail.com>
2016-02-18 16:23:05 -0500

    Fix relatedLink directive to process getByUrls function after newUrls are processed.

--- app/directives/relatedLinks.js
+++ app/directives/relatedLinks.js
@@ -32,26 +32,26 @@ angular.module('StarWarsApp')
 					scope.chunk = items.slice(slicePosition, slicePosition + 5);
 				};
 
-				scope.$watch('urls', function(newUrls){
-					factory.getByUrls(newUrls, function(err, data){
-						if(err){
-							scope.loaded = true;
-							scope.hasError = true;
-							scope.err = 'An error occured. Please try again later.';
-							return console.log(err);
-						} else if(data && data.length){
-							scope.loaded = true;
-							scope.hasData = true;
-							items = data;
-							scope.totalPages = Math.ceil(items.length / 5);
-							scope.chunk = items.slice(slicePosition, 5);
-						} else if(newUrls){
-							scope.loaded = true;
-						}
-
-					});
+				scope.$watch('urls', function(newUrls, oldValue){
+					if(newUrls !== oldValue){
+						factory.getByUrls(newUrls, function(err, data){
+							if(err){
+								scope.loaded = true;
+								scope.hasError = true;
+								scope.err = 'An error occured. Please try again later.';
+								return console.log(err);
+							} else if(data && data.length){
+								scope.loaded = true;
+								scope.hasData = true;
+								items = data;
+								scope.totalPages = Math.ceil(items.length / 5);
+								scope.chunk = items.slice(slicePosition, 5);
+							} else if(newUrls){
+								scope.loaded = true;
+							}
+						});
+					} // end if	
 				});
-
-			}
+			} // end return
 		};
 	});
