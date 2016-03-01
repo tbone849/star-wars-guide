befc54ec9310f72ce8cb6edf10639171bb07ac67
Tim Sarvey <timsarvey@gmail.com>
2016-03-01 18:06:38 -0500

    Fix watch logic for urls in relatedLinks directive. This should fix the infinite spinner bug.

--- app/directives/relatedLinks.js
+++ app/directives/relatedLinks.js
@@ -12,6 +12,8 @@ angular.module('StarWarsApp')
 				scope.currentPage = 1;
 				scope.hasError = false;
 				scope.hasData = false;
+				scope.loaded = false;
+				console.log(scope.loaded);
 				var factory = element.injector().get(attrs.factory);
 				var items = [];
 				var slicePosition = 0;
@@ -32,24 +34,32 @@ angular.module('StarWarsApp')
 					scope.chunk = items.slice(slicePosition, slicePosition + 5);
 				};
 
+				var getByUrls = function(urls){
+					factory.getByUrls(urls, function(err, data){
+						if(err){
+							scope.loaded = true;
+							scope.hasError = true;
+							scope.err = 'An error occured. Please try again later.';
+							return console.log(err);
+						} else if(data && data.length){
+							scope.loaded = true;
+							scope.hasData = true;
+							items = data;
+							scope.totalPages = Math.ceil(items.length / 5);
+							scope.chunk = items.slice(slicePosition, 5);
+						} else if(urls.length === 0) {
+							scope.loaded = true;
+						}
+					});
+				};
+
 				scope.$watch('urls', function(newUrls, oldValue){
-					if(newUrls !== oldValue){
-						factory.getByUrls(newUrls, function(err, data){
-							if(err){
-								scope.loaded = true;
-								scope.hasError = true;
-								scope.err = 'An error occured. Please try again later.';
-								return console.log(err);
-							} else if(data && data.length){
-								scope.loaded = true;
-								scope.hasData = true;
-								items = data;
-								scope.totalPages = Math.ceil(items.length / 5);
-								scope.chunk = items.slice(slicePosition, 5);
-							} else if(newUrls){
-								scope.loaded = true;
-							}
-						});
+					if(newUrls === oldValue){
+						if(typeof newUrls !== 'undefined'){
+							getByUrls(newUrls);
+						}
+					} else {
+						getByUrls(newUrls);
 					} // end if	
 				});
 			} // end return
